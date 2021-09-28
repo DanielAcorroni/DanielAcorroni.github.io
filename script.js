4cfef7e965b7c620c1ccca419eef39530f2912bf
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2021-09-28 18:18:25 -0300

    Adiciona código completo referente a mobile

---
+++ script.js
@@ -0,0 +1,57 @@
+const hamburguerMenu = document.querySelector('.container-menu')
+function changeDisplay() {
+    const myLinks = document.querySelector(".my-links");
+    if (myLinks.classList.contains('active')) {
+      myLinks.classList.remove('active');
+      myLinks.classList.add('unactive')
+      hamburguerMenu.classList.toggle("change");
+    } else {
+      myLinks.classList.remove('unactive');
+      myLinks.classList.add('active')
+      hamburguerMenu.classList.toggle("change");
+    }
+}
+
+const project01 = document.getElementById('project-01');
+const project02 = document.getElementById('project-02');
+const project03 = document.getElementById('project-03');
+const project04 = document.getElementById('project-04');
+
+function changeVisibleProject(projectToNone, projectToInline) {
+  projectToInline.classList.remove('project-not-select')
+  projectToInline.classList.add('project-select');
+  projectToNone.classList.remove('project-select')
+  projectToNone.classList.add('project-not-select')
+}
+
+function changeProjectsleft() {
+  if(project01.classList.contains('project-select')) {
+    changeVisibleProject(project01, project04);
+  } else if(project02.classList.contains('project-select')) {
+    changeVisibleProject(project02, project01);
+  } else if(project03.classList.contains('project-select')) {
+    changeVisibleProject(project03, project02);
+  } else if(project04.classList.contains('project-select')) {
+    changeVisibleProject(project04, project03);
+  }
+}
+
+function changeProjectsRight() {
+  if(project01.classList.contains('project-select')) {
+    changeVisibleProject(project01, project02);
+  } else if(project02.classList.contains('project-select')) {
+    changeVisibleProject(project02, project03);
+  } else if(project03.classList.contains('project-select')) {
+    changeVisibleProject(project03, project04);
+  } else if(project04.classList.contains('project-select')) {
+    changeVisibleProject(project04, project01);
+  }
+}
+
+const arrowLeftBtn = document.querySelector('#arrow-left')
+const arrowRightBtn = document.querySelector('#arrow-right')
+window.onload = () => {
+    hamburguerMenu.addEventListener('click', changeDisplay);
+    arrowLeftBtn.addEventListener('click', changeProjectsleft);
+    arrowRightBtn.addEventListener('click', changeProjectsRight);
+}
